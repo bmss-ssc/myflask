6016822001368bfb4e24528a7c6bc338524813a0
ssc <912012280@qq.com>
2019-04-21 10:18:02 +0800

    second

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 #### 环境介绍
 
 Python=3.6 flask=1.0.2
+
+#### 请求钩子
+
+before_first_request：注册一个函数，在处理第一个请求之前运行。
+before_request：注册一个函数，在每次请求之前运行。
+after_request：注册一个函数，如果没有未处理的异常抛出，在每次请求之后运行。
+teardown_request：注册一个函数，即使有未处理的异常抛出，也在每次请求之后运行。
--- hello.py
+++ hello.py
@@ -1,17 +1,25 @@
 from flask import Flask
+from flask import redirect  # 导入重定向
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
-@app.route('/')
+@app.route('/')  # 默认路由
 def index():
     return '<h1>Hello World</h1>'
 
 
-@app.route('/user/<name>')
+@app.route('/re')  # 重定向
+def re():
+    return redirect('https://www.hao123.com/')
+
+
+@app.route('/user/<name>')  # 可变参数路由写法
 def user(name):
     return '<h1>hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
